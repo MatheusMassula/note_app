b771a4a1409cf737bcfa9483d6da1d272cd7a0a3
Matheus Massula <matheusmassula1@gmail.com>
2020-03-30 22:52:31 -0300

    criar a prévia da nota

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/note_preview.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -8,7 +9,7 @@ class Home extends StatelessWidget {
         title: Text('Minhas notas'),
         centerTitle: true,
       ),
-      body: Text('Aqui ficará a suas notas'),
+      body: NotePreview(noteText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et porttitor ligula. Aliquam erat volutpat. Suspendisse eget aliquet eros. Donec commodo pharetra tellus, condimentum egestas velit sodales sed. Donec facilisis sem quis dapibus vulputate. Quisque iaculis viverra lacinia. Morbi dapibus lorem orci, sit amet vestibulum est vestibulum eget. Cras dictum ultrices vehicula. Mauris elementum non neque molestie euismod. Mauris at orci vel sapien feugiat ultricies. Curabitur sagittis pellentesque est, ac consectetur nunc hendrerit ac. Quisque ullamcorper enim odio, eu luctus libero ultricies sed. Cras a sollicitudin turpis, ut aliquet velit. Donec ut auctor nisl.'),
     );
   }
 }
